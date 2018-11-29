358ebb9d6e19b61d1e1aa63a33a54309af9ca64a
qiushaowei <qiushaowei@360.net>
2018-11-29 14:55:19 +0800

    up

--- prod.config.js
+++ prod.config.js
@@ -1,7 +1,7 @@
 // 不需要的字段就注释掉！
 module.exports = {
     host: 'vps.webfe.tech',
-    port: 8211,
+    port: 9211,
     // branch: ''
     // targetDir: 'public',
     f2e_version: new Date().getTime(),
